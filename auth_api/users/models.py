4f32bab2dd2af1175d27029f71015d4e017b85d2
Abdi Berhe <abdiberhe@gmail.com>
2025-05-11 12:29:17 +0300

    Revert "Add frontend"

--- auth_api/users/models.py
+++ auth_api/users/models.py
@@ -1,8 +1,9 @@
 from typing import ClassVar
 
 from django.contrib.auth.models import AbstractUser
-from django.core.mail import send_mail
-from django.db import models
+from django.db.models import CharField
+from django.db.models import EmailField
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from .managers import UserManager
@@ -16,12 +17,10 @@ class User(AbstractUser):
     """
 
     # First and last name do not cover name patterns around the globe
-    # e.g. Chinese, Indian, and Arabic names so use 'name' instead
-    # Setting first_name and last_name to None
-    # name = models.CharField(_("Name of User"), blank=True, max_length=255) # noqa: ERA001, E501
-    # first_name = None  # type: ignore[assignment] # noqa: ERA001
-    # last_name = None  # type: ignore[assignment] # noqa: ERA001
-    email = models.EmailField(_("email address"), unique=True)
+    name = CharField(_("Name of User"), blank=True, max_length=255)
+    first_name = None  # type: ignore[assignment]
+    last_name = None  # type: ignore[assignment]
+    email = EmailField(_("email address"), unique=True)
     username = None  # type: ignore[assignment]
 
     USERNAME_FIELD = "email"
@@ -29,48 +28,11 @@ class User(AbstractUser):
 
     objects: ClassVar[UserManager] = UserManager()
 
-    class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
-        ordering = ("-date_joined",)
+    def get_absolute_url(self) -> str:
+        """Get URL for user's detail view.
 
-    def clean(self):
-        super().clean()
-        self.email = self.__class__.objects.normalize_email(self.email)
+        Returns:
+            str: URL for user detail.
 
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        """Send an email to this user."""
-        send_mail(subject, message, from_email, [self.email], **kwargs)
-
-    def __str__(self):
-        return self.email
-
-    @property
-    def name(self):
         """
-        Dynamic 'name' property to provide compatibility with code expecting
-        a 'name' attribute(Eg. SocialAccountAdapter).
-        It uses the get_full_name() method.
-        """
-        return self.get_full_name()
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(
-        User,
-        related_name="profile",
-        on_delete=models.CASCADE,
-    )
-    bio = models.TextField(max_length=500, blank=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ("-user__date_joined",)
-
-    def __str__(self):
-        return self.user.email
-
-    def activeness(self):
-        if self.user.is_active:
-            return "Active"
-        return "Inactive"
+        return reverse("users:detail", kwargs={"pk": self.id})
--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,13 @@
-from django.urls import include
-from django.urls import path
+from django.conf import settings
+from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
-urlpatterns = [
-    path("", include("auth_api.users.api.urls")),
-    # Your stuff: custom urls includes go here
-]
+from auth_api.users.api.views import UserViewSet
+
+router = DefaultRouter() if settings.DEBUG else SimpleRouter()
+
+router.register("users", UserViewSet)
+
+
+app_name = "api"
+urlpatterns = router.urls
