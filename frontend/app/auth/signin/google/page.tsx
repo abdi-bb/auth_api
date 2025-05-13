8391c6dba2c8b02e9e986ab05aac4520967bca8c
Abdi Berhe <abdiberhe@gmail.com>
2025-05-13 22:05:50 +0300

    refactor: await searchParams in GoogleCallbackPage and simplify code handling

--- frontend/app/auth/signin/google/page.tsx
+++ frontend/app/auth/signin/google/page.tsx
@@ -3,15 +3,19 @@ import GoogleCallbackClient from "./google-callback-client"
 
 export const dynamic = "force-dynamic"
 
-export default function GoogleCallbackPage({
+export default async function GoogleCallbackPage({
     searchParams,
 }: {
     searchParams: { [key: string]: string | string[] | undefined }
 }) {
+    // In Next.js 15, searchParams needs to be awaited
+    const parseSearchParams = await searchParams
+    const code = parseSearchParams.code
+
     // If there's no code parameter, redirect to the signin page
-    if (!searchParams.code) {
+    if (!code) {
         redirect("/auth/signin")
     }
 
-    return <GoogleCallbackClient code={searchParams.code as string} />
+    return <GoogleCallbackClient code={code as string} />
 }
